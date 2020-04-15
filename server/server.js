fa687c4a15e98653e2f7b94d20df7945d405cd85
Nghĩa Hữu <buihuunghia.10hlvc@gmail.com>
2020-04-15 10:44:57 +0700

    edit .env

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({ extend: true }));
 app.use(bodyParser.json());
